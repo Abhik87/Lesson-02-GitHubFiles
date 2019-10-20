385265ff1403f66799d5536fddfcac03592d60a4
Ubuntu <abhik.it2011@gmail.com>
2019-10-20 07:29:02 +0000

    add multiple program files

---
+++ C_Program.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+
+int main(){
+
+
+
+printf(“Hello! I am C-Program. Thank you!”);
+
+
+
+return 0;
+
+
+
+}
---
+++ HelloJava.java
@@ -0,0 +1,17 @@
+class HelloJava {
+
+
+
+public static void main(String args[]){
+
+
+
+System.out.Println(“ I am your Java Program. Thank you! ”);
+
+
+
+}
+
+
+
+}
